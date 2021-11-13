03d613158b8bf9b55ffa001f89a1f6549b6e0b56
Alexande Becker <a.becker@cern.ch>
2021-11-13 20:32:50 +0000

    modified the template into the blink test project

--- rules.mk
+++ rules.mk
@@ -154,24 +154,24 @@ $(PROJECT).elf: $(OBJS) $(LDSCRIPT) $(LIBDEPS)
 %.list: %.elf
 	$(OBJDUMP) -S $< > $@
 
-%.flash: %.elf
-	@printf "  FLASH\t$<\n"
-ifeq (,$(OOCD_FILE))
-	$(Q)(echo "halt; program $(realpath $(*).elf) verify reset" | nc -4 localhost 4444 2>/dev/null) || \
-		$(OOCD) -f interface/$(OOCD_INTERFACE).cfg \
-		-f target/$(OOCD_TARGET).cfg \
-		-c "program $(realpath $(*).elf) verify reset exit" \
-		$(NULL)
-else
-	$(Q)(echo "halt; program $(realpath $(*).elf) verify reset" | nc -4 localhost 4444 2>/dev/null) || \
-		$(OOCD) -f $(OOCD_FILE) \
-		-c "program $(realpath $(*).elf) verify reset exit" \
-		$(NULL)
-endif
+#%.flash: %.elf
+#	@printf "  FLASH\t$<\n"
+#ifeq (,$(OOCD_FILE))
+#	$(Q)(echo "halt; program $(realpath $(*).elf) verify reset" | nc -4 localhost 4444 2>/dev/null) || \
+#		$(OOCD) -f interface/$(OOCD_INTERFACE).cfg \
+#		-f target/$(OOCD_TARGET).cfg \
+#		-c "program $(realpath $(*).elf) verify reset exit" \
+#		$(NULL)
+#else
+#	$(Q)(echo "halt; program $(realpath $(*).elf) verify reset" | nc -4 localhost 4444 2>/dev/null) || \
+#		$(OOCD) -f $(OOCD_FILE) \
+#		-c "program $(realpath $(*).elf) verify reset exit" \
+#		$(NULL)
+#endif
 
 clean:
 	rm -rf $(BUILD_DIR) $(GENERATED_BINS)
 
-.PHONY: all clean flash
+.PHONY: all clean
 -include $(OBJS:.o=.d)
 
